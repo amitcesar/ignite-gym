1035b2ae7a0cd43c2db8c19f7ebbe56df4570a74
Amit Cesar <amitcesar27@gmail.com>
2023-09-11 18:39:09 -0300

    style: screen home front end finished

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,31 +1,65 @@
-import { HStack, VStack } from "native-base";
-import { HomeHeader } from "@components/HomeHeader";
-import { Group } from "@components/Group";
 import { useState } from "react";
 
+import { HomeHeader } from "@components/HomeHeader";
+import { Group } from "@components/Group";
+import { HStack, VStack, FlatList, Heading, Text } from "native-base";
+import { ExerciseCard } from "@components/ExerciseCard";
+
 export function HomeScreen() {
-  const [groupSelected, setGroupSelected] = useState("costa");
+  const [groupSelected, setGroupSelected] = useState("costas");
+  const [groups, setGroups] = useState([
+    "Costas",
+    "Biceps",
+    "Triceps",
+    "ombro",
+  ]);
+  const [exercises, setExercises] = useState([
+    "Puxada Frontal",
+    "Remada unilateral",
+    "Remada curvada",
+  ]);
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <HStack>
-        <Group
-          name="costa"
-          isActive={groupSelected === "costa"}
-          onPress={() => setGroupSelected("costa")}
-        />
 
-        <Group
-          name="biceps"
-          isActive={groupSelected === "biceps"}
-          onPress={() => setGroupSelected("biceps")}
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={
+              groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()
+            }
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent={"space-between"} mb={5}>
+          <Heading color="gray.200" fontSize={"md"}>
+            Exercicio
+          </Heading>
+
+          <Text color="gray.200" fontSize={"sm"}>
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => <ExerciseCard />}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20 }}
         />
-        <Group
-          name="ombro"
-          isActive={groupSelected === "ombro"}
-          onPress={() => setGroupSelected("ombro")}
-        />
-      </HStack>
+      </VStack>
     </VStack>
   );
 }
