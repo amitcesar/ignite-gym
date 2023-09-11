e998256ba4402345cbb379592e9330ab01e4e5b7
Amit Cesar <amitcesar27@gmail.com>
2023-09-11 20:17:03 -0300

    frontend profile Screen

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -21,7 +21,7 @@ export function ProfileScreen() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
-      <ScrollView>
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={6} px={10}>
           {photoIsLoading ? (
             <Skeleton
@@ -53,10 +53,14 @@ export function ProfileScreen() {
             placeholder="erickcloud@email.com"
             isDisabled
           />
-        </Center>
 
-        <VStack px={10} mt={12} mb={9}>
-          <Heading color="gray.200" fontSize={"md"} mb={2}>
+          <Heading
+            color="gray.200"
+            fontSize={"md"}
+            alignSelf="flex-start"
+            mb={2}
+            mt={12}
+          >
             Alterar senha
           </Heading>
 
@@ -70,7 +74,7 @@ export function ProfileScreen() {
           />
 
           <Button title="Atualizar" mt={4} />
-        </VStack>
+        </Center>
       </ScrollView>
     </VStack>
   );
