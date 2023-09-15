7dc3c2e172ea39f4c0704eb9c3536bda4068ceee
Amit Cesar <amitcesar27@gmail.com>
2023-09-15 15:50:53 -0300

    feat: persist login with asyncstorage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,7 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { UserDTO } from "@dtos/UserDTO";
 import { api } from "@services/api";
+import { storageGetUser, storageUserSave } from "@storage/storageUser";
 
 export type AuthContextDataProps = {
   user: UserDTO;
@@ -26,11 +27,24 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       });
       if (data.user) {
         setUser(data.user);
+        storageUserSave(data.user);
       }
     } catch (error) {
       throw error;
     }
   }
+
+  const loadUserData = async () => {
+    const userLogged = await storageGetUser();
+    if (userLogged) {
+      setUser(userLogged);
+    }
+  };
+
+  useEffect(() => {
+    loadUserData();
+  }, []);
+
   return (
     <AuthContext.Provider
       value={{
---
+++ src/storage/storageUser.ts
@@ -0,0 +1,16 @@
+import { UserDTO } from "@dtos/UserDTO";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { USER_STORAGE } from "./storageConfig";
+
+export const storageUserSave = async (user: UserDTO) => {
+  await AsyncStorage.setItem(USER_STORAGE, JSON.stringify(user))
+}
+
+
+export const storageGetUser = async () => {
+  const storage = await AsyncStorage.getItem(USER_STORAGE);
+
+  const user: UserDTO = storage ? JSON.parse(storage) : {};
+
+  return user
+}
